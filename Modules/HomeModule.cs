09a2b43a5360eeeea0b565254ec2b443e9bb3ca8
Nicholas Mead <nixter11@uw.edu>
2017-03-03 10:23:52 -0800

    add routes for band and venue management

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+using Tracker.Objects;
+
+namespace Tracker
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+        Get["/"] =_=> {
+            return View["index.cshtml"];
+        };
+
+        Get["/venues"] =_=> {
+            return View["venue-management.cshtml", Venue.GetAll()];
+        };
+
+        Get["/bands"] =_=> {
+            return View["band-management-cshtml", Band.GetAll()];
+        };
+    }
+  }
+}
